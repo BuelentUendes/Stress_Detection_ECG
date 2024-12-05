1ff2b89b19b40a63a0bb355ed9ea50ace4c06368
BuelentUendes <buelent.tt@web.de>
2024-12-05 17:20:43 +0100

    made some changes to allow for downsampled data

--- sia/preprocessors/neurokit.py
+++ sia/preprocessors/neurokit.py
@@ -1,7 +1,66 @@
 import warnings
 import neurokit2 as nk
+from neurokit2 import ecg_peaks, signal_rate, ecg_quality, ecg_delineate, ecg_phase
+import pandas as pd
 
 from typing import Callable
+
+
+def preprocessing_pipeline(ecg_cleaned, sampling_rate: int = 1000, method: str = "neurokit"):
+    """
+    Runs through the preprocessing pipeline for an already cleaned signal
+    :param signal: Cleaned ECG signal
+    :return: pd dataframe
+    """
+
+    # Detect R-peaks
+    instant_peaks, info = ecg_peaks(
+        ecg_cleaned=ecg_cleaned,
+        sampling_rate=sampling_rate,
+        method=method,
+        correct_artifacts=True,
+    )
+
+    # Calculate heart rate
+    rate = signal_rate(
+        info, sampling_rate=sampling_rate, desired_length=len(ecg_cleaned)
+    )
+
+    # Assess signal quality
+    quality = ecg_quality(
+        ecg_cleaned, rpeaks=info["ECG_R_Peaks"], sampling_rate=sampling_rate
+    )
+
+    # Merge signals in a DataFrame
+    signals = pd.DataFrame(
+        {
+            "ECG_Clean": ecg_cleaned,
+            "ECG_Rate": rate,
+            "ECG_Quality": quality,
+        }
+    )
+
+    # Delineate QRS complex
+    delineate_signal, delineate_info = ecg_delineate(
+        ecg_cleaned=ecg_cleaned, rpeaks=info["ECG_R_Peaks"], sampling_rate=sampling_rate
+    )
+    info.update(delineate_info)  # Merge waves indices dict with info dict
+
+    # Determine cardiac phases
+    cardiac_phase = ecg_phase(
+        ecg_cleaned=ecg_cleaned,
+        rpeaks=info["ECG_R_Peaks"],
+        delineate_info=delineate_info,
+    )
+
+    # Add additional information to signals DataFrame
+    signals = pd.concat(
+        [signals, instant_peaks, delineate_signal, cardiac_phase], axis=1
+    )
+
+    # return signals DataFrame and R-peak locations
+    return signals, info
+
 
 def pantompkins(sampling_rate: int = 1000) -> Callable[[list], dict]:
     """Compute ECG features using the Pan-Tompkins algorithm.
@@ -83,14 +142,17 @@ def engzeemod(sampling_rate: int = 1000) -> Callable[[list], dict]:
         return df.to_dict('list')
     return inner
 
-def neurokit(sampling_rate: int = 1000) -> Callable[[list], dict]:
+def neurokit(sampling_rate: int = 1000, clean_before_processing: bool = True) -> Callable[[list], dict]:
     """Compute ECG features using the NeuroKit algorithm.
 
     Parameters
     ----------
     sampling_rate : int, optional
         The sampling rate of the ECG signal, by default 1000.
-    
+    clean_before_processing : bool, optional
+        If the signal needs to get cleaned before processing.
+        In our dataset, the original sample frequency is at 1000 Hz which is not yet cleaned.
+
     Returns
     -------
     function
@@ -98,7 +160,15 @@ def neurokit(sampling_rate: int = 1000) -> Callable[[list], dict]:
     """
     def inner(signal: list[float]): 
         warnings.filterwarnings("ignore")
-        df, _ = nk.ecg_process(signal, sampling_rate=sampling_rate, method='neurokit')
+        if clean_before_processing:
+            df, _ = nk.ecg_process(signal, sampling_rate=sampling_rate, method='neurokit')
+        else:
+            df, _ = preprocessing_pipeline(
+                signal,
+                sampling_rate=sampling_rate,
+                method="neurokit"
+            )
+
         warnings.filterwarnings("default")
         return df.to_dict('list')
     return inner
