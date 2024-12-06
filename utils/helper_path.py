06c0153392946c8cadd44fdfdc17e239a25a5c23
BuelentUendes <buelent.tt@web.de>
2024-12-06 15:30:55 +0100

    added a new feature path

--- utils/helper_path.py
+++ utils/helper_path.py
@@ -7,5 +7,6 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 BASE_PATH = abspath(os.path.join(FILE_PATH, './../'))
 DATA_PATH = abspath(os.path.join(BASE_PATH, './', "data"))
 CLEANED_DATA_PATH = abspath(os.path.join(DATA_PATH, "cleaned"))
+FEATURE_DATA_PATH = abspath(os.path.join(DATA_PATH, "features"))
 RAW_DATA_PATH = abspath(os.path.join(DATA_PATH, "raw", "Raw ECG project"))
 
