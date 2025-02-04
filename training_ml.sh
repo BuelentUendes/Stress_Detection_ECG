83e6cff63398f20500a6c1918dec494cab4420ae
BuelentUendes <buelent.tt@web.de>
2025-02-04 10:19:31 +0100

    fixedn a typo

--- training_ml.sh
+++ training_ml.sh
@@ -3,14 +3,14 @@
 # shellcheck disable=SC2068
 # We set the number of trials to 25 optuna
 models=("lr" "rf" "xgboost" "knn")
-window_size=(60 30 10) # we use four different window sizes
+window_size=(60) # we use four different window sizes
 negative_classes=("baseline" "low_physical_activity" "moderate_physical_activity")
 
 for model in ${models[@]}
 do
   for window_len in ${window_size[@]}
   do
-    for class in ${negative_class[@]}
+    for class in ${negative_classes[@]}
     do
       python3 main_training.py --model_type $model --window_size "$window_len" --negative_class $class --n_trials 25
     done
