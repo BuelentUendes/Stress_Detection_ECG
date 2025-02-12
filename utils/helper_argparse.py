7b9894b91a10afeecf2dbe91575dd14fa5c69e0b
BuelentUendes <buelent.tt@web.de>
2025-02-12 16:10:16 +0100

    refactored the code and added an argparse helper file

---
+++ utils/helper_argparse.py
@@ -0,0 +1,38 @@
+import argparse
+
+
+def validate_scaler(value: str) -> str:
+    if value.lower() not in ["standard_scaler", "min_max", None]:
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value}. Choose from 'standard_scaler' or 'min_max'.")
+    return value.lower()
+
+
+def validate_category(value: str) -> str:
+    valid_categories = ['high_physical_activity', 'mental_stress', 'baseline',
+                        'low_physical_activity', 'moderate_physical_activity']
+    if value.lower() not in valid_categories:
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value}. "
+                                         f"Choose from options in {valid_categories}.")
+    return value.lower()
+
+
+def validate_target_metric(value: str) -> str:
+    if value.lower() not in ["roc_auc", "accuracy"]:
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value}. Choose from 'standard_scaler' or 'min_max'.")
+    return value.lower()
+
+
+def validate_ml_model(value: str) -> str:
+    valid_ml_models = ['dt', 'rf', 'adaboost', 'lda', 'knn', 'lr', 'xgboost', 'qda', 'random_baseline', 'svm']
+    if value.lower() not in valid_ml_models:
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value}. "
+                                         f"Choose from options in {valid_ml_models}.")
+    return value.lower()
+
+
+def validate_resampling_method(value: str) -> str:
+    implemented_sampling_methods = ['upsample', 'downsample', 'smote', 'none', 'adasyn']
+    if value.lower() not in implemented_sampling_methods:
+        raise argparse.ArgumentTypeError(f"Invalid choice: {value}. "
+                                         f"Choose from options in {implemented_sampling_methods}.")
+    return value.lower()
