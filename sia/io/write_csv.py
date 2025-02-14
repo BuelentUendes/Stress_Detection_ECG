a1b84c36314ce91098b04bcd68e872e51c239047
BuelentUendes <buelent.tt@web.de>
2025-02-14 09:11:16 +0100

    option to write to csv file or parquet file

--- sia/io/write_csv.py
+++ sia/io/write_csv.py
@@ -7,18 +7,20 @@ from datasets import Dataset, IterableDataset
 
 from typing import Callable, Union
 
-def write_csv(path: str) -> Callable[[str, Union[Dataset, IterableDataset]], None]:
-    """Save data to a CSV file.
+def write_csv(path: str, use_parquet: bool = True) -> Callable[[str, Union[Dataset, IterableDataset]], None]:
+    """Save data to a CSV file or parquet file.
     
     Parameters
     ----------
     path : str
         The path to the CSV file.
+    use_parquet : bool, optional
+        If True, saves data in parquet format. If False, saves as CSV. Default is False.
 
     Returns
     -------
     Callable[[str, Union[Dataset, IterableDataset]], None]
-        A function that saves the data to a CSV file
+        A function that saves the data to a CSV or parquet file
     """
     def inner(filename: str, ds: Union[Dataset, IterableDataset]) -> None:
         warnings.filterwarnings("ignore")
@@ -35,7 +37,13 @@ def write_csv(path: str) -> Callable[[str, Union[Dataset, IterableDataset]], Non
             filename = number_match.group(1)
 
         location.parent.mkdir(parents=True, exist_ok=True)
-        output_path_pq = location.parent / f"{filename}.parquet"
-        ds.to_parquet(output_path_pq)
+        
+        if use_parquet:
+            output_path = location.parent / f"{filename}.parquet"
+            ds.to_parquet(output_path)
+        else:
+            output_path = location.parent / f"{filename}.csv"
+            ds.to_csv(output_path)
+            
         warnings.filterwarnings("default")
     return inner
