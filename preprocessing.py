9d61c46008b86059527be9339672139ca3a42515
BuelentUendes <buelent.tt@web.de>
2025-02-19 15:26:54 +0100

    s

--- preprocessing.py
+++ preprocessing.py
@@ -11,9 +11,12 @@ from sklearn.preprocessing import LabelEncoder
 
 from utils.helper_path import RAW_DATA_PATH, CLEANED_DATA_PATH
 from utils.helper_functions import create_directory
+import time
 
 
 def main(args):
+
+    start_time = time.time()
 
     if args.data_chunk == 1:
         input_path = os.path.join(RAW_DATA_PATH, str(args.sample_frequency), "part_1")
@@ -25,6 +28,9 @@ def main(args):
         input_path = os.path.join(RAW_DATA_PATH, str(args.sample_frequency), "part_4")
     elif args.data_chunk == 5:
         input_path = os.path.join(RAW_DATA_PATH, str(args.sample_frequency), "part_5")
+    # As of now we allow all processing only for sample frequency 1000
+    elif args.data_chunk == -1 and args.sample_frequency == 1000:
+        input_path = os.path.join(RAW_DATA_PATH, str(args.sample_frequency))
 
     input_file = str(args.participant_number) + "_LAB_Conditions_ECG.edf" if args.participant_number != -1 else "*.edf"
     print(f"we are processing {input_file}" if args.participant_number != -1 else f"we are processing all files")
@@ -54,7 +60,9 @@ def main(args):
         .process(neurokit(sampling_rate=args.sample_frequency, method=args.method)) \
         .filter(lambda label: [_label != "Lying_supine" for _label in label]) \
         .filter(lambda ECG_Quality: [quality > .25 for quality in ECG_Quality]) \
-        .to(write_csv(os.path.join(output_path, '[0-9]{5}.csv.')))
+        .to(write_csv(os.path.join(output_path, '[0-9]{5}.csv.'), use_parquet=True))
+
+    print("--- %s minutes ---" % ((time.time() - start_time)/60))
 
 
 if __name__ == "__main__":
@@ -70,7 +78,7 @@ if __name__ == "__main__":
                         default="neurokit")
     parser.add_argument("--number_processors", type=int, default=1, help="If set to -1, it uses all available")
     parser.add_argument("--participant_number", type=int, help="which specific number to run. Set -1 for all",
-                        default=-1)
+                        default=30100)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--data_chunk", type=int, default=1,
                         help="Which data chunk to process. 1 for part 1, 2 for part 2 etc.")
@@ -82,3 +90,5 @@ if __name__ == "__main__":
 
     main(args)
 
+    # Todo:
+    #Do this completely
