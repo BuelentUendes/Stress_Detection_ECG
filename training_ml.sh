a7e0b8a064634495e20d1396b0a9f102d6f4a733
BuelentUendes <buelent.tt@web.de>
2025-02-20 08:50:52 +0100

    wrote a simple bash script to run the experiments

--- training_ml.sh
+++ training_ml.sh
@@ -3,16 +3,20 @@
 # shellcheck disable=SC2068
 # We set the number of trials to 25 optuna
 models=("lr" "rf" "xgboost")
-sample_frequencies=(128 ) # we use four different window sizes
+sample_frequencies=(128 256 512 1000) # we use four different window sizes
 negative_classes=("baseline" "low_physical_activity" "moderate_physical_activity")
 
 for model in ${models[@]}
 do
-  for window_len in ${window_size[@]}
+  for frequency in ${sample_frequencies[@]}
   do
     for class in ${negative_classes[@]}
     do
-      python3 main_training.py --model_type $model --window_size "$window_len" --negative_class $class --n_trials 25
+      if [ "@class" != "baseline" ]; then
+          python3 main_training.py --model_type $model --window_size $frequency --negative_class $class --do_hyperparameter_tuning --n_trials 25 --add_calibration_plots --bootstrap_test_results --verbose
+      else
+        python3 main_training.py --model_type $model --window_size $frequency --negative_class $class --do_hyperparameter_tuning --n_trials 25 --add_calibration_plots --bootstrap_test_results --verbose --resampling_method smote
+      fi
     done
   done
 done
