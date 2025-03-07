28a662a9604088d786c586a27f43ce56eef835ec
BuelentUendes <buelent.tt@web.de>
2025-03-07 15:23:45 +0100

    there was a small bug

--- training_ml.sh
+++ training_ml.sh
@@ -2,8 +2,8 @@
 
 # shellcheck disable=SC2068
 # We set the number of trials to 25 optuna
-models=("lr" "rf" "xgboost")
-sample_frequencies=(128 256 512 1000) # we use four different window sizes
+models=("lr" "xgboost" "rf") # Random forest is expensive
+sample_frequencies=(1000 512 256 128) # we use four different window sizes # For now we do it 1000
 negative_classes=("baseline" "low_physical_activity" "moderate_physical_activity")
 
 for model in ${models[@]}
@@ -12,10 +12,10 @@ do
   do
     for class in ${negative_classes[@]}
     do
-      if [ "@class" = "baseline" ]; then
-        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --do_hyperparameter_tuning --n_trials 25 --add_calibration_plots --bootstrap_test_results --verbose
+      if [ "$class" = "baseline" ]; then
+        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --add_calibration_plots --bootstrap_test_results --verbose --bootstrap_subcategories --do_hyperparameter_tuning --n_trials 25
       else
-        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --do_hyperparameter_tuning --n_trials 25 --add_calibration_plots --bootstrap_test_results --verbose --resampling_method smote
+        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --add_calibration_plots --bootstrap_test_results --verbose --resampling_method smote --do_hyperparameter_tuning --n_trials 25 --bootstrap_subcategories
       fi
     done
   done
