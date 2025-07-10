1ded3261920474ec2724832283aa06dfb2b82b0a
BuelentUendes <buelent.tt@web.de>
2025-07-10 09:53:14 +0200

    add the training_ml bash script as well

--- training_ml.sh
+++ training_ml.sh
@@ -2,9 +2,9 @@
 
 # shellcheck disable=SC2068
 # We set the number of trials to 25 optuna
-models=("lr" "xgboost" "rf") # Random forest is expensive
-sample_frequencies=(1000 512 256 128) # we use four different window sizes # For now we do it 1000
-negative_classes=("baseline" "low_physical_activity" "moderate_physical_activity")
+models=("lr" "xgboost") # Random forest is expensive
+sample_frequencies=(1000 500 250 125) # we use four different window sizes # For now we do it 1000
+negative_classes=("base_lpa_mpa")
 
 for model in ${models[@]}
 do
@@ -15,7 +15,7 @@ do
       if [ "$class" = "baseline" ]; then
         python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --add_calibration_plots --bootstrap_test_results --verbose --bootstrap_subcategories --do_hyperparameter_tuning --n_trials 25
       else
-        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --add_calibration_plots --bootstrap_test_results --verbose --resampling_method smote --do_hyperparameter_tuning --n_trials 25 --bootstrap_subcategories
+        python3 main_training.py --model_type $model --sample_frequency $frequency --negative_class $class --add_calibration_plots --bootstrap_test_results --verbose --resampling_method smote --bootstrap_subcategories --do_hyperparameter_tuning --n_trials 25
       fi
     done
   done
